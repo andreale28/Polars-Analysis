773ba1cbea26f22c79bc2afa75e1ad1d5bbf3050
andreale28 <lhson43680@gmail.com>
2023-09-30 22:38:23 +0700

    create a new etl pipeline with pyarrow, polars and delta lake.

---
+++ src/polars_ingestion.py
@@ -0,0 +1,146 @@
+import os
+from typing import Iterable
+
+import dotenv
+import duckdb
+import polars as pl
+import pyarrow.parquet as parquet
+import s3fs
+from deltalake import write_deltalake, DeltaTable
+
+
+def load_s3_env_file(
+        envs: list[str]
+) -> None:
+    """
+    Check environment variables for S3
+    :param envs:
+    :return:
+    """
+    dotenv.load_dotenv()
+
+    for var in envs:
+        if not os.getenv(var):
+            raise ValueError(
+                f"Required environment variables are not set correctly: {var}"
+            )
+
+    return None
+
+
+def pyarrow_ingestion(table_name: str) -> None:
+    """
+
+    :param table_name: str, name of the table to ingest
+    :return: None
+    """
+    required_key = [
+        "AWS_DEFAULT_REGION",
+        "AWS_ACCESS_KEY_ID",
+        "AWS_SECRET_ACCESS_KEY",
+        "LOCAL_FILE_NAME",
+        "S3_BUCKET",
+    ]
+    load_s3_env_file(envs=required_key)
+    s3_region = os.getenv('AWS_DEFAULT_REGION')
+    s3_access_key_id = os.getenv('AWS_ACCESS_KEY_ID')
+    s3_secret_access_key = os.getenv('AWS_SECRET_ACCESS_KEY')
+    s3_bucket_name = os.getenv("S3_BUCKET")
+    s3_local_file_name = os.getenv("LOCAL_FILE_NAME")
+    storage_options = {
+        'aws_default_region': s3_region,
+        'aws_access_key_id': s3_access_key_id,
+        'aws_secret_access_key': s3_secret_access_key,
+    }
+
+    delta_table_path = table_name
+
+    cloud_fs = s3fs.S3FileSystem(
+        key=s3_access_key_id,
+        secret=s3_secret_access_key,
+    )
+
+    with cloud_fs.open(f"s3://{s3_bucket_name}/data/{s3_local_file_name}", "rb") as f:
+        arrow_table = parquet.read_table(f)
+
+    write_deltalake(
+        data=arrow_table,
+        table_or_uri=delta_table_path,
+        mode='overwrite',
+        overwrite_schema=True
+    )
+
+    return None
+
+
+def read_delta(
+        table_name: str,
+        columns: Iterable[str]
+) -> pl.LazyFrame:
+    """
+    Lazily read deltalake table from disk to LazyFrame with rename
+    :param table_name:
+    :param columns: columns to optimize with z-orders
+    :return: LazyFrame
+    """
+    dt = DeltaTable(table_uri=table_name)
+
+    # print(f"Schema of our data is \n {dt.schema().to_pyarrow()}")
+
+    try:
+        dt.optimize.compact()
+        dt.optimize.z_order(columns=columns)
+    except Exception as e:
+        print(f"Error when optimize table as {e}")
+
+    rename_dict = {
+        '1st_deliver_attempt': 'first_deliver_attempt',
+        '2nd_deliver_attempt': 'second_deliver_attempt',
+        'buyeraddress': 'buyer_address',
+        'selleraddress': 'seller_address',
+    }
+    df = (
+        pl
+        .scan_delta(source=table_name)
+        .rename(mapping=rename_dict)
+    )
+
+    return df
+
+
+def export_s3(df: pl.DataFrame,
+              s3_region: str,
+              s3_access_key_id: str,
+              s3_secret_access_key: str,
+              s3_bucket_name,
+              s3_file_name: str
+              ):
+    """
+    export polars.DataFrame to S3 bucket using DuckDB
+    :param df:
+    """
+
+    output = df
+    conn = duckdb.connect(':memory:')
+    conn.sql(
+        f"""
+                INSTALL httpfs;
+                LOAD httpfs;
+                PRAGMA enable_optimizer;
+                SET s3_region='{s3_region}';
+                SET s3_access_key_id='{s3_access_key_id}';
+                SET s3_secret_access_key='{s3_secret_access_key}';
+            """
+    )
+
+    conn.sql("SELECT * FROM output;").to_table("output")
+    conn.sql(
+        f"""
+            COPY
+                output
+            TO
+                's3://{s3_bucket_name}/data/{s3_file_name}'(FORMAT 'PARQUET');
+            """
+    )
+
+    return None
---
+++ src/polars_run.py
@@ -0,0 +1,101 @@
+import os
+import time
+
+import patito as pt
+import s3fs
+
+from polars_ingestion import pyarrow_ingestion, read_delta
+from schemas import (
+    Input, Output
+)
+from transforms import tweak_result
+
+
+def check_file_exists(
+        bucket_name: str,
+        file_name: str,
+        key: str,
+        secret: str
+) -> bool:
+    """Check if a file exists in S3 storage.
+
+    Args:
+        bucket_name (str): The name of the S3 bucket.
+        file_name (str): The name of the file to check.
+        key (str): access key to s3
+        secret (str): secret access key to s3
+
+    Returns:
+        bool: True if the file exists, False otherwise.
+    """
+
+    fs = s3fs.S3FileSystem(
+        anon=True,
+        key=key,
+
+        secret=secret
+    )
+    return fs.exists(
+        f's3://{bucket_name}/data/{file_name}'
+    )
+
+
+def main():
+
+    start_time = time.perf_counter()
+
+    table = "march_order"
+    columns = ["orderid", "pick"]
+
+    pyarrow_ingestion(
+        table_name=table
+    )
+
+    df = read_delta(
+        table,
+        columns=columns
+    )
+
+    try:
+        Input.validate(
+            df.collect()
+        )
+    except pt.ValidationError as e:
+        print(e)
+
+    output, result = tweak_result(df)
+
+    try:
+        Output.validate(output)
+    except pt.ValidationError as e:
+        print(e)
+
+    print(f"Percentage of late delivered orders {result.collect()}")
+
+    s3_region = os.getenv('AWS_DEFAULT_REGION')
+    s3_access_key_id = os.getenv('AWS_ACCESS_KEY_ID')
+    s3_secret_access_key = os.getenv('AWS_SECRET_ACCESS_KEY')
+    s3_bucket_name = os.getenv("S3_BUCKET")
+    s3_file_name = os.getenv("S3_FILE_NAME")
+
+    cloud_fs = s3fs.S3FileSystem(
+        key=s3_access_key_id,
+        secret=s3_secret_access_key,
+    )
+
+    if not check_file_exists(
+            s3_bucket_name,
+            s3_file_name,
+            s3_access_key_id,
+            s3_secret_access_key
+    ):
+        with cloud_fs.open(f"s3://{s3_bucket_name}/data/{s3_file_name}", "wb") as f:
+            output.write_parquet(f)
+
+    print(
+        f"Execution time: {time.perf_counter() - start_time} seconds"
+    )
+
+
+if __name__ == "__main__":
+    main()
